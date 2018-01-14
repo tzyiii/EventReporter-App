a2b6a5b6cf33bd8493e22302b4ef7b879e373539
tzyiii <zhengyt@uci.edu>
2018-01-13 19:43:28 -0800

    more new features

--- app/src/main/java/com/laioffer/eventreporter/Event.java
+++ app/src/main/java/com/laioffer/eventreporter/Event.java
@@ -12,6 +12,71 @@ public class Event {
     private String title;
     private String address;
     private String description;
+    private int like;
+    private String id;
+    private long time;
+    private String username;
+    private String imgUri;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() { return this.title; }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() { return this.address; }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() { return this.description; }
+
+    public int getLike() {
+        return like;
+    }
+
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
+
+    public Event() {}
 
     /**
      * Constructor
@@ -21,12 +86,5 @@ public class Event {
         this.address = address;
         this.description = description;
     }
-
-    /**
-     * Getters for private attributes of Event class.
-     */
-    public String getTitle() { return this.title; }
-    public String getAddress() { return this.address; }
-    public String getDescription() { return this.description; }
 }
 
--- app/src/main/java/com/laioffer/eventreporter/MainActivity.java
+++ app/src/main/java/com/laioffer/eventreporter/MainActivity.java
@@ -1,41 +1,95 @@
 package com.laioffer.eventreporter;
 
+
+import android.content.Intent;
 import android.content.res.Configuration;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements EventFragment.OnItemSelectListener {
-    private EventFragment mListFragment;
-    private CommentFragment mGridFragment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-
-
+public class MainActivity extends AppCompatActivity {
+    private EditText mUsernameEditText;
+    private EditText mPasswordEditText;
+    private Button mSubmitButton;
+    private Button mRegisterButton;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("Life cycle test", "we are at on creat");
-        // Show different fragments based on screen size.
-        if (findViewById(R.id.fragment_container) != null) {
-            Fragment fragment = isTablet() ? new CommentFragment() : new EventFragment();
-            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
-        //add list view
-        mListFragment = new EventFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.event_container, mListFragment).commit();
-        //add Gridview
 
-        if (isTablet()) {
-            mGridFragment = new CommentFragment();
-            getSupportFragmentManager().beginTransaction().add(R.id.comment_container, mGridFragment).commit();
-        }
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+        // Firebase uses singleton to initialize the sdk
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mUsernameEditText = (EditText) findViewById(R.id.editTextLogin);
+        mPasswordEditText = (EditText) findViewById(R.id.editTextPassword);
+        mSubmitButton = (Button) findViewById(R.id.submit);
+        mRegisterButton = (Button) findViewById(R.id.register);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = mUsernameEditText.getText().toString();
+                final String password = Utils.md5Encryption(mPasswordEditText.getText().toString());
+                final User user = new User(username, password, System.currentTimeMillis());
+                mDatabase.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild(username)) {
+                            Toast.makeText(getBaseContext(),"username is already registered, please change one", Toast.LENGTH_SHORT).show();
+                        } else if (!username.equals("") && !password.equals("")){
+                            // put username as key to set value
+                            mDatabase.child("users").child(user.getUsername()).setValue(user);
+                            Toast.makeText(getBaseContext(),"Successfully registered", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+            }
+        });
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String username = mUsernameEditText.getText().toString();
+                final String password = Utils.md5Encryption(mPasswordEditText.getText().toString());
+                mDatabase.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChild(username) && (password.equals(dataSnapshot.child(username).child("password").getValue()))) {
+                            Log.i( " Your log", "You successfully login");
+                            Intent myIntent = new Intent(MainActivity.this, EventActivity.class);
+                            Utils.username = username;
+                            startActivity(myIntent);
+
+                        } else {
+                            Toast.makeText(getBaseContext(),"Please login again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+            }
+        });
+
 
     }
+
+
 
     private boolean isTablet() {
         return (getApplicationContext().getResources().getConfiguration().screenLayout &
@@ -87,11 +141,5 @@ public class MainActivity extends AppCompatActivity implements EventFragment.OnI
         super.onDestroy();
         Log.e("Life cycle test", "We are at onDestroy()");
     }
-
-    @Override
-    public void onItemSelected(int position){
-        mGridFragment.onItemSelected(position);
-    }
-
 
 }
